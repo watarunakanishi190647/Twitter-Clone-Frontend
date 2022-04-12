b9c4d5983efee7db47bb7f1d744e0a46c4327fe8
watarunakanishi190647 <95616173+watarunakanishi190647@users.noreply.github.com>
2022-04-12 22:41:17 +0900

    Herokuのドメインに変えた

--- apollo-client.tsx
+++ apollo-client.tsx
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://localhost:8080/graphql",
+    // uri: "http://localhost:8080/graphql",
+    uri: "https://still-ravine-42221.herokuapp.com",
     cache: new InMemoryCache(),
 });
 
