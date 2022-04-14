a163a3eeee419c1a62fb58132ba11758131a5058
watarunakanishi190647 <95616173+watarunakanishi190647@users.noreply.github.com>
2022-04-14 17:07:22 +0900

    "/"を入れた

--- apollo-client.tsx
+++ apollo-client.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
     // uri: "http://localhost:8080/graphql",
-    uri: "https://still-ravine-42221.herokuapp.com",
+    uri: "https://still-ravine-42221.herokuapp.com/",
     cache: new InMemoryCache(),
 });
 
